886f2cb498a89cefd35008557b9f8318c79c6309
Sandeep Sharma <sandeep.sharma@tigeranalytics.com>
2022-11-15 16:09:58 +0530

    commented payload

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ try {
   const message = `Running evaluation on ${branchName}`;
   core.setOutput("message", message);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  //   const payload = JSON.stringify(github.context.payload, undefined, 2);
+  //   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
