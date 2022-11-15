f75ff7bf70c3bce8881e8faa74e5bee78d9e4a96
Sandeep Sharma <sandeep.sharma@tigeranalytics.com>
2022-11-15 17:51:41 +0530

    add pr comment part

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ const github = require("@actions/github");
 
 try {
   const payload = github.context.payload;
+  const prNumber = payload.pull_request.number;
+  const prBody = payload.pull_request.body;
+  const prTitle = payload.pull_request.title;
+  const github_token = core.getInput("token");
+  const octokit = new github.GitHub(github_token);
+  if (prTitle === null) {
+    octokit.issues.createComment({
+      ...github.context.repo,
+      issue_number: prNumber,
+      body: "PR title is missing",
+    });
+    core.setFailed("PR title is not provided");
+  }
+  if (prBody === null) {
+    octokit.issues.createComment({
+      ...github.context.repo,
+      issue_number: prNumber,
+      body: "PR title is missing",
+    });
+    core.setFailed("PR Description is not provided");
+  }
   const baseBranchName = payload.pull_request.base.ref;
   const headBranchName = payload.pull_request.head.ref;
   console.log(`Got PR to merge from ${headBranchName} -> ${baseBranchName}`);
