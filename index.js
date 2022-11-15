87f8600195f63796bc688766da2bf5c86b6c5126
Sandeep Sharma <sandeep.sharma@tigeranalytics.com>
2022-11-15 18:44:31 +0530

    added payload log

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const github = require("@actions/github");
 
 try {
   const payload = github.context.payload;
+  console.log(payload);
   const prNumber = payload.pull_request.number;
   const prBody = payload.pull_request.body;
   const prTitle = payload.pull_request.title;
   const github_token = core.getInput("token");
   const octokit = github.getOctokit(github_token);
+  const baseBranchName = payload.pull_request.base.ref;
+  const headBranchName = payload.pull_request.head.ref;
+  console.log(`Got PR to merge from ${headBranchName} -> ${baseBranchName}`);
+  // check if PR title is provided
   if (prTitle === null) {
     octokit.rest.issues.createComment({
       ...github.context.repo,
@@ -16,17 +21,16 @@ try {
     });
     core.setFailed("PR title is not provided");
   }
+  // check if PR description is provided
   if (prBody === null) {
     octokit.rest.issues.createComment({
       ...github.context.repo,
       issue_number: prNumber,
-      body: "PR title is missing",
+      body: "PR Description is missing",
     });
     core.setFailed("PR Description is not provided");
   }
-  const baseBranchName = payload.pull_request.base.ref;
-  const headBranchName = payload.pull_request.head.ref;
-  console.log(`Got PR to merge from ${headBranchName} -> ${baseBranchName}`);
+
   const message = `Running evaluation on ${headBranchName}`;
   core.setOutput("message", message);
 } catch (error) {
